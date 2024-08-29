b668ebe6ed92652e42c78ca900c4f07f9a47c844
Marco Gullo <marco.gullo@apify.com>
2024-08-29 11:12:14 +0200

    setup playwright browser server

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,54 @@
 import http from 'http';
-// Apify SDK - toolkit for building Apify Actors (Read more at https://docs.apify.com/sdk/js/)
-import { Actor } from 'apify';
+import { Actor, log } from 'apify';
+import { chromium } from 'playwright';
+import httpProxy from 'http-proxy';
+import stream from 'stream'
 
-// this is ESM project, and as such, it requires you to specify extensions in your relative imports
-// read more about this here: https://nodejs.org/docs/latest-v18.x/api/esm.html#mandatory-file-extensions
-// note that we need to use `.js` even when inside TS files
-// import { router } from './routes.js';
-
-// The init() call configures the Actor for its environment. It's recommended to start every Actor with an init()
 await Actor.init();
 
-// Create a simple HTTP server that will respond with a message
+async function spawnAndProxy(req: http.IncomingMessage, socket: stream.Duplex, head: Buffer) {
+    const browserServer = await chromium.launchServer({ headless: false });
+    const wsEndpoint = browserServer.wsEndpoint();
+
+    socket.once('close', async () => {
+        log.info('Socket closed, closing browser server', { wsEndpoint });
+        await browserServer.close();
+    });
+
+    log.info('Spawned browser, creating proxy', { wsEndpoint });
+
+    // If we don't do this, connection is refused with HTTP error 400 and socket error 1006
+    delete req.headers.origin;
+    req.url = '';
+
+    const proxy = httpProxy.createProxyServer()
+    proxy.ws(
+        req,
+        socket,
+        head,
+        {
+            changeOrigin: true,
+            target: wsEndpoint,
+            ws: true,
+        },
+        async (error) => {
+          log.error('Error creating proxy, closing browser server', { wsEndpoint, error });
+          await browserServer.close();
+        },
+    )
+}
+
+const port = Actor.config.get('standbyPort') || process.env.ACTOR_WEB_SERVER_PORT || 3000;
+
 const server = http.createServer((req, res) => {
+    log.info('Request received', req.headers);
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello from Actor Standby!\n');
+    res.end(`Hello from Actor Standby! Build: ${Actor.getEnv().actorBuildNumber}\n`);
 });
 
-// Listen on the standby port
-server.listen(Actor.config.get('standbyPort'));
+server.listen(port, () => log.info('Server is listening', { port }));
+
+server.on('upgrade', async (req: http.IncomingMessage, socket: stream.Duplex, head: Buffer) => {
+    log.info('WS request', req.headers);
+    await spawnAndProxy(req, socket, head);
+})
